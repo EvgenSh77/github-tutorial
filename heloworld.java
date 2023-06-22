35067de2966ea23051231ba931e737cfc0411a6a
Evgen Shramkov <evgenshramkov@gmail.com>
2023-06-22 21:48:08 +0300

    add text
    додаємо текст

---
+++ heloworld.java
@@ -0,0 +1,18 @@
+public class heloworld {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+         System.out.println ("Hello, World, and beautiful, good people!");
+        // виводимо на консоль слова, Hello, World, and good people
+        // виводимо на конслоль слова українською "Привіт свід, і гарні люди
+        // тут знаходиться опис класу, методу або інший фрагмент коду
+
+        //* Тут знаходиться опис класу, методу або іншого фрагменту коду
+
+        System.out.println("Привіт сівіт, та всі прекрасні, добрі люди!");
+    }
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
